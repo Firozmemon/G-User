0ea693a127fe4ea6c56f8a8281485209c86dc581
Firozmemon <firozmemon0@gmail.com>
2017-08-05 21:34:48 +0530

    initial basic setup

--- app/src/main/java/com/firozmemon/g_user/MainActivity.java
+++ app/src/main/java/com/firozmemon/g_user/MainActivity.java
@@ -1,13 +1,43 @@
 package com.firozmemon.g_user;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.noDataFound)
+    TextView noDataFound;
+
+    @OnClick(R.id.fab)
+    void onFabClicked() {
+        Snackbar.make(coordinatorLayout, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+
     }
+
 }
